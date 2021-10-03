0f6c03e6118897e96142b37d4ac125d340e19fda
framo-alb <47555931+framo-alb@users.noreply.github.com>
2021-10-03 07:24:44 +0000

    Createed project and devcontainer

---
+++ todos/src/main/java/no/alb/dat250/expass4/rest/App.java
@@ -0,0 +1,13 @@
+package no.alb.dat250.expass4.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
